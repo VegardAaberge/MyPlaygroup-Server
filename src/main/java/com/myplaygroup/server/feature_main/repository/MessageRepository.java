392b180cdc1a52b5a66cadfcdba697926f716185
VegardAaberge <vegard.aaberge@gmail.com>
2022-05-11 10:09:14 +0800

    Improve SQL formating

--- src/main/java/com/myplaygroup/server/feature_main/repository/MessageRepository.java
+++ src/main/java/com/myplaygroup/server/feature_main/repository/MessageRepository.java
@@ -18,17 +18,19 @@ public interface MessageRepository extends JpaRepository<Message, Long> {
     @Query(
             nativeQuery = true,
             value = "SELECT DISTINCT " +
-                    "message.id, " +
-                    "message.created, " +
-                    "message.message, " +
-                    "app_user.username as createdBy, " +
-                    "app_user.profile_name as profileName " +
+                    "    message.id, " +
+                    "    message.created, " +
+                    "    message.message, " +
+                    "    app_user.username as createdBy, " +
+                    "    app_user.profile_name as profileName " +
                     "FROM message " +
-                    "JOIN message_receivers " +
-                    "    ON message.id = message_receivers.message_id " +
-                    "JOIN app_user " +
-                    "    ON app_user.id = message.app_user_id " +
-                    "WHERE message.app_user_id = ?1 OR message_receivers.receivers_id = ?1"
+                    "    JOIN message_receivers  " +
+                    "        ON message.id = message_receivers.message_id " +
+                    "    JOIN app_user " +
+                    "        ON app_user.id = message.app_user_id " +
+                    "WHERE " +
+                    "    message.app_user_id = ?1 " +
+                    "    OR message_receivers.receivers_id = ?1"
     )
     Optional<List<MessageResponse>> findByOwnerAndReceiver(Long userID);
 }
