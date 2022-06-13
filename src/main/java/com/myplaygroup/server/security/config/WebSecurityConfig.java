78fcaa30bfc6c880d586937cbdeb373b85c1349b
VegardAaberge <vegard.aaberge@gmail.com>
2022-06-13 14:26:13 +0800

    Update the WebSecurityConfig

--- src/main/java/com/myplaygroup/server/security/config/WebSecurityConfig.java
+++ src/main/java/com/myplaygroup/server/security/config/WebSecurityConfig.java
@@ -39,7 +39,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         http.authorizeRequests().antMatchers("/chat-socket").permitAll();
         http.authorizeRequests().antMatchers("/api/v*/login/**", "/api/v*/reset-password/**").permitAll();
         http.authorizeRequests().antMatchers("/api/v*/chat/**", "/api/v*/profile/**", "api/v1/schedule/**").hasAnyAuthority(ADMIN.name(), USER.name());
-        http.authorizeRequests().antMatchers(HttpMethod.POST,"/api/v*/registration/**", "api/v1/classes/**", "api/v1/plans/**").hasAuthority(ADMIN.name());
+        http.authorizeRequests().antMatchers("/api/v*/users/**", "api/v1/classes/**", "api/v1/plans/**").hasAuthority(ADMIN.name());
         http.authorizeRequests().anyRequest().authenticated();
         http.addFilter(customAuthenticationFilter);
         http.addFilterBefore(new CustomAuthorizationFilter(authorizationService), UsernamePasswordAuthenticationFilter.class);
